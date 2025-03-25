73537dc7e9441e4d0f4af8f8d90ad84c1aa9544b
Atinkene <massinasylvanusbassene@gmail.com>
2025-03-25 01:21:31 +0000

    Fix serve command and pin version to 14.2.1 new3

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 require('dotenv').config();
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+const API_URL = "https://dbplateformebackend.up.railway.app";
 
 const api = axios.create({
   baseURL: API_URL,
